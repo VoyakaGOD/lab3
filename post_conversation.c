71f2aaf654cea51dc250546c193deb5de4ebd70d
Alexander Udeneev <mrprofu361@gmail.com>
2023-02-13 16:52:20 +0300

    first ver

---
+++ post_conversation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *post_conversion(char *S);
+
+int main()
+{
+    char S[64] = "2 + 2";
+
+    return 0;
+}
+
+
+char *post_conversion (char* S)
+{   
+    assert(S == NULL);
+
+    char *new_string = (char*) calloc(256, sizeof(char));
+    char elem = 'A';
+
+    while (elem != '\0')
+    {
+
+    }
+}
