08440f44ce8f58ef70e14c9c395644d6a6e2a010
phrolow <111210693+phrolow@users.noreply.github.com>
2023-02-13 17:16:22 +0300

    Update intstack.cpp

--- intstack.cpp
+++ intstack.cpp
@@ -1,17 +1,38 @@
 #include "intstack.h"
 
 void push_int (struct Node_int **top, int a) {
-    (*top)->number = a;
+    Node_int *next = (Node_int *) malloc(sizeof(Node_int));
 
-    (*top)->next = (Node_int *) malloc(sizeof(Node_int));
+    next->number = a;
+    next->prev = (*top);
+    next->next = NULL;
 
-    *top = (*top)->next;
+    if(*top) {
+        (*top)->next = next;
+    }
 
-    (*top)->number = POISON;
+    (*top) = next;
 }
 
 int pop_int (struct Node_int **top) {
+    int res = (*top)->number;
+    Node_int *prev = (*top)->prev;
 
+    (*top)->number = POISON;
+    (*top)->prev = NULL;
+    (*top)->next = NULL;
+
+    free(*top);
+
+    (*top) = prev;
+
+    if(prev) {
+        prev->next = NULL;
+    }
+
+    return res;
 }
 
-bool isempty_int(struct Node_int *top);
+bool isempty_int(struct Node_int *top) {
+    return top->number == POISON;
+}
